36cb7785390d4b831ae1c05e307785c5978fac5f
heyscottn <heyscottn@gmail.com>
2023-12-30 10:15:25 -0600

    edits

--- my_gui.py
+++ my_gui.py
@@ -2,11 +2,11 @@ import tkinter as tk
 
 def click_button1(name):
     root = tk.Tk()
-    root.wm_geometry("200x200")
+    root.wm_geometry("600x600")
     root.title("My GUI")
     frame2 = tk.Frame(root)
     frame2.grid(row=0, column=0, sticky='news')
-    label2 = tk.Label(frame2, text="hello " + name)
+    label2 = tk.Label(frame2, text="hello " + name, font=('Arial', 20))
     label2.pack()
     frame2.tkraise()
 
@@ -19,12 +19,12 @@ def gui_hello():
     # create frame 1
     frame1 = tk.Frame(root)
     frame1.grid(row=0, column=0, sticky='news')
-    label1 = tk.Label(frame1, text="what's your name?")
+    label1 = tk.Label(frame1, text="what's your name?", font=('Arial', 20))
     label1.pack()
     # create text box
-    tb = tk.Entry(frame1, bd=3)
+    tb = tk.Entry(frame1, bd=3, font=('Arial', 20))
     tb.pack(pady=5)
-    btn1 = tk.Button(frame1, text='enter', command=lambda: click_button1(tb.get()))
+    btn1 = tk.Button(frame1, text='enter', font=('Arial', 20), command=lambda: click_button1(tb.get()))
     btn1.pack()
 
     frame2 = tk.Frame(root)
